7921e8156fffef8fb1618554da11b7fe6c2de1c1
Igor Angelo Martins <igoram1511@hotmail.com>
2026-09-22 18:57:35 -0300

    adiciona rotas para as páginas de login e home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+
+
+const Home: React.FC = () => {
+
+    return (
+        <>
+            <h1 className="text-2xl font-bold mb-4"> TESTE HOME</h1>
+
+        </>
+    )
+}
+
+export default Home
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,13 @@
+
+
+const Login: React.FC = () => {
+
+    return (
+        <>
+            <h1 className="text-2xl font-bold mb-4"> TESTE Login</h1>
+
+        </>
+    )
+}
+
+export default Login
